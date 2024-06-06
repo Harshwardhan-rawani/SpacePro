47d3c11bd11f66d4b15a916123ef8ac231365779
Harshwardhan <harshwardhan.rawani@gmail.com>
2024-06-06 23:56:22 +0530

    third

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -23,12 +23,12 @@ function Photo() {
          <h1 className='font-bold sm:text-lg md:text-xl lg:text-2xl mt-5 text-center  text-white'>Satelite</h1>
          <p className='mt-4 italic text-justify px-4  text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo porro, a temporibus repudiandae eos cupiditate et adipisci, dolor voluptas laudantium libero, nemo quas dolore. Voluptate repellat odio incidunt corporis iusto quaerat et vel illum at rerum voluptatum quibusdam laboriosam nostrum repel</p>
         </div>
-        <div className='lg:w-1/3 w-full my-6 shadow-lg rounded-md px-4 py-2 hover:bg-gray-900 border-b-2 border-r-2' data-aos="zoom-in" data-aos-delay="200">
+        <div className='lg:w-1/3 w-full my-6 shadow-lg rounded-md px-4 py-2 hover:bg-gray-900 border-b-2 border-r-2' data-aos="zoom-in" data-aos-delay="50">
          <img src={space5} alt="" className='h-72 w-full rounded-t-md shadow-lg ' />
          <h1 className='font-bold sm:text-lg md:text-xl lg:text-2xl mt-5 text-center text-white'>Space Ship</h1>
          <p className='mt-4 italic text-justify px-4 text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo porro, a temporibus repudiandae eos cupiditate et adipisci, dolor voluptas laudantium libero, nemo quas dolore. Voluptate repellat odio incidunt corporis iusto quaerat et vel illum at rerum voluptatum quibusdam laboriosam nostrum repel</p>
         </div>
-        <div className='lg:w-1/3 w-full my-6 shadow-lg rounded-md px-4 py-2 hover:bg-gray-900 border-b-2 border-r-2' data-aos="zoom-in" data-aos-delay="400">
+        <div className='lg:w-1/3 w-full my-6 shadow-lg rounded-md px-4 py-2 hover:bg-gray-900 border-b-2 border-r-2' data-aos="zoom-in" data-aos-delay="100">
          <img src={space6} alt="" className='h-72 w-full rounded-t-md shadow-lg ' />
          <h1 className='font-bold sm:text-lg md:text-xl lg:text-2xl mt-5 text-center text-white'>Astronaut</h1>
          <p className='mt-4 italic text-justify px-4 text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo porro, a temporibus repudiandae eos cupiditate et adipisci, dolor voluptas laudantium libero, nemo quas dolore. Voluptate repellat odio incidunt corporis iusto quaerat et vel illum at rerum voluptatum quibusdam laboriosam nostrum repel</p>
--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,11 +1,28 @@
 import { IoRocket } from "react-icons/io5";
 import { FaUserAlt } from "react-icons/fa";
+import { useEffect, useState } from "react";
 function  Top() {
 
+  const [scrolled, setScrolled] = useState(false);
 
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 520) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
     <>
-        <div className='flex justify-between px-10 py-3 items-center  fixed w-screen z-50 bg-[#161616dd]'>
+        <div className={`flex justify-between px-10 py-3 items-center  fixed w-screen z-50 ${scrolled?"bg-[#161616dd]":""} `}>
           <div className='flex items-center lg:text-xl md:text-lg sm:text-md text-white font-bold'>
             <IoRocket className='-rotate-45'/> H-Space</div>
           <button className='text-white border-2 p-2 rounded-md'>Login</button>
