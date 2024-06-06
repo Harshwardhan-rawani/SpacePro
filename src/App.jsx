ce786c0982120e3170aeed47896dc5eaa0db6ceb
Harshwardhan <harshwardhan.rawani@gmail.com>
2024-06-06 21:07:16 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-
+import 'flowbite';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Layout from './components/Layout';
